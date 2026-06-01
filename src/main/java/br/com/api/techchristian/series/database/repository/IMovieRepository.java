322294001788c901d80b37b5e6d4bd92b6a36639
Christian <christianx111.ls@gmail.com>
2026-06-01 08:37:05 -0300

    create repository

---
+++ src/main/java/br/com/api/techchristian/series/database/repository/IMovieRepository.java
@@ -0,0 +1,9 @@
+package br.com.api.techchristian.series.database.repository;
+
+import br.com.api.techchristian.series.database.models.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface IMovieRepository extends JpaRepository<Movie, UUID> {
+}
