1ac9b3702748efb5466e5df6309334ca4321f4db
Christian <christianx111.ls@gmail.com>
2026-06-02 22:09:04 -0300

    create method findByTitle

--- src/main/java/br/com/api/techchristian/series/database/repository/IMovieRepository.java
+++ src/main/java/br/com/api/techchristian/series/database/repository/IMovieRepository.java
@@ -3,7 +3,9 @@ package br.com.api.techchristian.series.database.repository;
 import br.com.api.techchristian.series.database.models.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface IMovieRepository extends JpaRepository<Movie, UUID> {
+    Optional<Movie> findByTitle(String title);
 }
