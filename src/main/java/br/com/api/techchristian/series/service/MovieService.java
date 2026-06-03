deb6e452ac59f6e2237f8c8c590e2eea68ccce79
Christian <christianx111.ls@gmail.com>
2026-06-02 22:10:01 -0300

    implements code for create new movie

--- src/main/java/br/com/api/techchristian/series/service/MovieService.java
+++ src/main/java/br/com/api/techchristian/series/service/MovieService.java
@@ -1,4 +1,29 @@
 package br.com.api.techchristian.series.service;
 
+import br.com.api.techchristian.series.database.models.Movie;
+import br.com.api.techchristian.series.database.repository.IMovieRepository;
+import br.com.api.techchristian.series.dto.MovieDto;
+import br.com.api.techchristian.series.mappers.MovieMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class MovieService {
+    private final IMovieRepository movieRepository;
+
+    @Transactional
+    public Movie save(MovieDto.Create dto) {
+
+        Movie movie = movieRepository.findByTitle(dto.title()).orElse(null);
+
+        if (movie != null) {
+            throw new IllegalArgumentException("Movie already exists");
+        }
+
+        Movie newMovie = MovieMapper.toEntity(dto);
+        return movieRepository.save(newMovie);
+    }
 }
