0ac72658c7e8791d03fab182657c5e793f98dc00
Christian <christianx111.ls@gmail.com>
2026-06-04 20:03:01 -0300

    update if

--- src/main/java/br/com/api/techchristian/series/service/MovieService.java
+++ src/main/java/br/com/api/techchristian/series/service/MovieService.java
@@ -5,7 +5,6 @@ import br.com.api.techchristian.series.database.repository.IMovieRepository;
 import br.com.api.techchristian.series.dto.MovieDto;
 import br.com.api.techchristian.series.mappers.MovieMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,12 +16,9 @@ public class MovieService {
     @Transactional
     public Movie save(MovieDto.Create dto) {
 
-        Movie movie = movieRepository.findByTitle(dto.title()).orElse(null);
-
-        if (movie != null) {
-            throw new IllegalArgumentException("Movie already exists");
-        }
-
+     if(movieRepository.findByTitle(dto.title()).isPresent()){
+         throw new IllegalArgumentException("Movie already exists.");
+     }
         Movie newMovie = MovieMapper.toEntity(dto);
         return movieRepository.save(newMovie);
     }
