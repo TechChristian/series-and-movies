7c14ff87f04f1c2174166fb6d981e805fc0bdd9b
Christian <christianx111.ls@gmail.com>
2026-06-04 20:08:57 -0300

    create class of global erros

---
+++ src/main/java/br/com/api/techchristian/series/handler/GlobalExceptionHandler.java
@@ -0,0 +1,9 @@
+package br.com.api.techchristian.series.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+}
