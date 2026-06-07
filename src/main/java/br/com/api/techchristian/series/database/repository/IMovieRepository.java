539fc6fba66250493bc989833c95ecbee0be3491
Christian <christianx111.ls@gmail.com>
2026-06-07 01:57:56 -0300

    add new searchs in database

--- src/main/java/br/com/api/techchristian/series/database/repository/IMovieRepository.java
+++ src/main/java/br/com/api/techchristian/series/database/repository/IMovieRepository.java
@@ -1,13 +1,16 @@
 package br.com.api.techchristian.series.database.repository;
 
+import br.com.api.techchristian.series.database.enums.GenreEnum;
 import br.com.api.techchristian.series.database.models.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface IMovieRepository extends JpaRepository<Movie, UUID> {
     boolean existsByTitle(String title);
-
     Optional<Movie> findByTitle(String title);
+    boolean existsByGenre(GenreEnum genre);
+    List<Movie> findByGenre(GenreEnum genre);
 }
