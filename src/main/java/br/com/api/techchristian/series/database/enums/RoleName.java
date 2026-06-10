fade3592f57be6ff550e998e068dba265d42dd01
Christian <christianx111.ls@gmail.com>
2026-06-10 07:57:15 -0300

    create models for users and roles and their relationships between tables

---
+++ src/main/java/br/com/api/techchristian/series/database/enums/RoleName.java
@@ -0,0 +1,6 @@
+package br.com.api.techchristian.series.database.enums;
+
+public enum Role {
+    USER("user"),
+    ADMIN("admin")
+}
---
+++ src/main/java/br/com/api/techchristian/series/database/models/User.java
@@ -0,0 +1,47 @@
+package br.com.api.techchristian.series.database.models;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private LocalDateTime createdAt;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<RolesEntity> roles = new HashSet<>();
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = LocalDateTime.now();
+    }
+
+}
