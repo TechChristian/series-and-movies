780550267a3f1b090f9d6995c819656eed36163a
Christian <christianx111.ls@gmail.com>
2026-06-13 22:23:39 -0300

    create mapper where entity transforms into dto for response user

--- src/main/java/br/com/api/techchristian/series/mappers/UserMapper.java
+++ src/main/java/br/com/api/techchristian/series/mappers/UserMapper.java
@@ -1,4 +1,14 @@
 package br.com.api.techchristian.series.mappers;
 
+import br.com.api.techchristian.series.database.models.User;
+import br.com.api.techchristian.series.dto.UserDto;
+
 public class UserMapper {
+
+    public static UserDto.UserLoginDto toResponseDto(User user){
+        return new  UserDto.UserLoginDto(
+                user.getEmail(),
+                user.getName()
+        );
+    }
 }
