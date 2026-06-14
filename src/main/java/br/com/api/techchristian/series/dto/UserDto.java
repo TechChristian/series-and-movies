c1ea35ca664e247ea2b61e84ed9195f8496ef35a
Christian <christianx111.ls@gmail.com>
2026-06-13 22:22:24 -0300

    create user dto for response

---
+++ src/main/java/br/com/api/techchristian/series/dto/UserDto.java
@@ -0,0 +1,30 @@
+package br.com.api.techchristian.series.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class UserDto {
+    public record UserRegisterDto(
+            @NotBlank(message = "name is required.")
+            String name,
+
+            @NotBlank(message = "email is required.")
+            @Email(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "Invalid email format")
+            String email,
+
+            @NotBlank(message = "Password is required.")
+            @Size(min = 6, max = 12, message = "Your password must be between 6 and 12  digits.")
+            String password
+    ){}
+
+    public record UserLoginDto(
+            @NotBlank(message = "email is required.")
+            @Email(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "Invalid email format")
+            String email,
+
+            @NotBlank(message = "Password is required.")
+            String password
+    ){}
+
+}
---
+++ src/main/java/br/com/api/techchristian/series/mappers/UserMapper.java
@@ -0,0 +1,4 @@
+package br.com.api.techchristian.series.mappers;
+
+public class UserMapper {
+}
