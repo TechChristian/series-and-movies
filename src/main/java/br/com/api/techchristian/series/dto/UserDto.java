6fcd30778e29a0f0dc8a9eb806c2fa149c332141
Christian <christianx111.ls@gmail.com>
2026-06-15 21:45:41 -0300

    create response for user

--- src/main/java/br/com/api/techchristian/series/dto/UserDto.java
+++ src/main/java/br/com/api/techchristian/series/dto/UserDto.java
@@ -4,6 +4,8 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+import java.util.UUID;
+
 public class UserDto {
     public record UserRegisterDto(
             @NotBlank(message = "name is required.")
@@ -16,6 +18,13 @@ public class UserDto {
             @NotBlank(message = "Password is required.")
             @Size(min = 6, max = 12, message = "Your password must be between 6 and 12  digits.")
             String password
+    ){}
+    public record UserResponseDto(
+            UUID id,
+
+            String name,
+
+            String email
     ){}
 
     public record UserLoginDto(
--- src/main/java/br/com/api/techchristian/series/mappers/UserMapper.java
+++ src/main/java/br/com/api/techchristian/series/mappers/UserMapper.java
@@ -5,8 +5,9 @@ import br.com.api.techchristian.series.dto.UserDto;
 
 public class UserMapper {
 
-    public static UserDto.UserLoginDto toResponseDto(User user){
-        return new  UserDto.UserLoginDto(
+    public static UserDto.UserResponseDto toResponseDto(User user){
+        return new UserDto.UserResponseDto(
+                user.getId(),
                 user.getEmail(),
                 user.getName()
         );
