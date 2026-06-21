2cd036e61abe381eb9cc06d99f9a60623e886745
Christian <christianx111.ls@gmail.com>
2026-06-21 19:51:20 -0300

    create controller and service for list all users

--- src/main/java/br/com/api/techchristian/series/controller/UserController.java
+++ src/main/java/br/com/api/techchristian/series/controller/UserController.java
@@ -1,4 +1,29 @@
 package br.com.api.techchristian.series.controller;
 
+import br.com.api.techchristian.series.dto.UserDto;
+import br.com.api.techchristian.series.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/api/users")
+
 public class UserController {
+    private final UserService userService;
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping
+    public ResponseEntity<List<UserDto.UserResponseDto>> getAllUsers(){
+        List<UserDto.UserResponseDto> responseList = userService.listAllUsers();
+        return ResponseEntity.status(HttpStatus.OK).body(responseList);
+    }
+
 }
--- src/main/java/br/com/api/techchristian/series/service/UserService.java
+++ src/main/java/br/com/api/techchristian/series/service/UserService.java
@@ -1,4 +1,30 @@
 package br.com.api.techchristian.series.service;
 
+import br.com.api.techchristian.series.database.models.User;
+import br.com.api.techchristian.series.database.repository.IUserRepository;
+import br.com.api.techchristian.series.dto.UserDto;
+import br.com.api.techchristian.series.mappers.UserMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+
 public class UserService {
+    private final IUserRepository userRepository;
+
+    @Transactional(readOnly = true)
+    public List<UserDto.UserResponseDto> listAllUsers() {
+        List<User> users = userRepository.findAll();
+
+        if(users.isEmpty()) {throw new UsernameNotFoundException("User not found");}
+
+        return UserMapper.toResponseDto(users);
+
+    }
+
 }
