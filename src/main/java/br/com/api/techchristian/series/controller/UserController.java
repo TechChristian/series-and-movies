246f41acc99afac5b9abc463c7e2eb60e8da59fd
Christian <christianx111.ls@gmail.com>
2026-06-22 08:46:35 -0300

    create controller for delete user of the ID and add feature for authorizations

--- src/main/java/br/com/api/techchristian/series/controller/UserController.java
+++ src/main/java/br/com/api/techchristian/series/controller/UserController.java
@@ -1,16 +1,16 @@
 package br.com.api.techchristian.series.controller;
 
+import br.com.api.techchristian.series.dto.MessageResponseDto;
 import br.com.api.techchristian.series.dto.UserDto;
 import br.com.api.techchristian.series.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,6 +24,14 @@ public class UserController {
     public ResponseEntity<List<UserDto.UserResponseDto>> getAllUsers(){
         List<UserDto.UserResponseDto> responseList = userService.listAllUsers();
         return ResponseEntity.status(HttpStatus.OK).body(responseList);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> userDelete(@PathVariable UUID id){
+        userService.deleteUser(id);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
 }
