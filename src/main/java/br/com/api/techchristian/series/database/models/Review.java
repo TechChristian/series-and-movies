b6725e74846d6b583917b7440074ff05e06e6e94
Christian <christianx111.ls@gmail.com>
2026-06-25 00:09:39 -0300

    create entity review

---
+++ src/main/java/br/com/api/techchristian/series/database/models/Review.java
@@ -0,0 +1,37 @@
+package br.com.api.techchristian.series.database.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "reviews")
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private Integer rating;
+
+    @Column(nullable = false)
+    private String comment;
+
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Movie movie;
+
+    @PrePersist
+    public void prePersist() {createdAt = LocalDateTime.now();}
+}
