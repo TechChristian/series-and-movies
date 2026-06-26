26da973879f6297d27937b9dc63c6ad9ce2dd0c1
Christian <christianx111.ls@gmail.com>
2026-06-26 08:10:16 -0300

    add controller for reviews

---
+++ src/main/java/br/com/api/techchristian/series/controller/ReviewController.java
@@ -0,0 +1,28 @@
+package br.com.api.techchristian.series.controller;
+
+import br.com.api.techchristian.series.dto.ReviewDto;
+import br.com.api.techchristian.series.service.ReviewService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("v1/api/movies")
+@RequiredArgsConstructor
+public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    @PostMapping("/{movieId}/reviews")
+    public ResponseEntity<ReviewDto.Response> addReview(@PathVariable UUID movieId, @RequestBody @Valid ReviewDto.Create create){
+
+        ReviewDto.Response response = reviewService.createReview(movieId, create);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+
+    }
+}
