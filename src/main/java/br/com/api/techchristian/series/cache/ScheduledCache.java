d5483f0b97fc3932f5395b3537fbd62116a1fa2c
Christian <christianx111.ls@gmail.com>
2026-07-02 17:33:20 -0300

    create one scheduled for cleaning of cache

--- src/main/java/br/com/api/techchristian/series/cache/ScheduledCache.java
+++ src/main/java/br/com/api/techchristian/series/cache/ScheduledCache.java
@@ -1,4 +1,21 @@
 package br.com.api.techchristian.series.cache;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
+
+@Component
+@Slf4j
 public class ScheduledCache {
+    @CacheEvict(value = "movies", allEntries = true)
+    @Scheduled(fixedRate = 30, timeUnit = TimeUnit.SECONDS)
+    public void evictMovies() {
+        log.info("clear cache... " + LocalDateTime.now());
+
+    }
+
 }
